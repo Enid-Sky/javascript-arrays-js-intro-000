a7bc5cc59291f3fb33ebf309eff392f0c3cd44fb
enid svymbersky <enid.bersky@gmail.com>
2019-12-06 20:53:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,10 +14,45 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 
 function addElementToEndOfArray(array, element) {
   return [...array, element];
+<<<<<<< HEAD
 
   
 }
+=======
+>>>>>>> 722e9cfbdbe7f843f9f6c8cbc6f02da66df4d432
 
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element);
+  return array;
+  
+}
+function accessElementInArray(array, index) {
+  
+  return array [index];
+  
+}
+
+<<<<<<< HEAD
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray (array) {
+  array = array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray (array) {
+  
+  array = array.slice(0, array.length -1);
+=======
 function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element);
   return array;
@@ -41,12 +76,7 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
-  array.pop();
-  return array;
-}
-
-function removeElementFromEndOfArray (array) {
-  
-  array = array.slice(0, array.length -1);
+  return array.pop();
+>>>>>>> 722e9cfbdbe7f843f9f6c8cbc6f02da66df4d432
   return array;
 }
